7562a55fec3fb4ca874039906d9f9a527c258778
Matt Griffiths <mgriff1984@gmail.com>
2018-09-12 14:49:19 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+copy_setup_file ()
+{
+    FILENAME=$1 #input file
+    FILE=$HOME/$2 #output file
+
+    if [ -f "$FILE" ]
+    then
+	    echo "Warning: $FILE already exists, please merge files manually."
+    else
+	    echo "Copying $FILE"
+        cp $FILENAME $HOME/.
+    fi
+}
+
+echo "Copying files ..."
+copy_setup_file bash_profile.local .bash_profile
+copy_setup_file vimrc.local .vimrc
+copy_setup_file inputrc.local .inputrc
+echo "finished"
+

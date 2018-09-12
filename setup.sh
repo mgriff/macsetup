1fd39a67d48124f4b3a594f7b7a97937ac7e8d4e
Matt Griffiths <mgriff1984@gmail.com>
2018-09-12 18:44:12 +0100

    changed to allow other folders

--- setup.sh
+++ setup.sh
@@ -2,21 +2,21 @@
 
 copy_setup_file ()
 {
-    FILENAME=$1 #input file
-    FILE=$HOME/$2 #output file
+    INPUT_FILE_PATH=$1
+    OUTPUT_FILE_PATH=$2
 
-    if [ -f "$FILE" ]
+    if [ -f "$OUTPUT_FILE_PATH" ]
     then
-	    echo "Warning: $FILE already exists, please merge files manually."
+	    echo "Warning: $OUTPUT_FILE_PATH already exists, please merge files manually."
     else
-	    echo "Copying $FILE"
-        cp $FILENAME $HOME/.
+	    echo "Copying $OUTPUT_FILE_PATH"
+        cp $INPUT_FILE_PATH $OUTPUT_FILE_PATH
     fi
 }
 
 echo "Copying files ..."
-copy_setup_file bash_profile.local .bash_profile
-copy_setup_file vimrc.local .vimrc
-copy_setup_file inputrc.local .inputrc
+copy_setup_file bash_profile.local $HOME/.bash_profile
+copy_setup_file vimrc.local $HOME/.vimrc
+copy_setup_file inputrc.local $HOME/.inputrc
 echo "finished"
 
